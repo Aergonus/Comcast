72040af237d3bd9319bfd1263884cc416767d217
pockygom <johnjaeryungsong@msn.com>
2016-03-14 00:35:32 -0400

    Create host.h

---
+++ host.h
@@ -0,0 +1,20 @@
+#ifndef HOST_H
+#define HOST_H
+
+#include <vector>
+#include <string>
+#include "link.h"
+#include "flow.h"
+#include "packet.h"
+#include "node.h"
+
+using namespace std;
+
+class host : public node {
+    void rcv_pkt(packet * inc_packet);
+	  vector<int> pkts_rcvd;
+
+public:
+	  host();
+
+};
