71aa1537b02bb02d839d183f2482f35c7218bf76
pockygom <johnjaeryungsong@msn.com>
2016-03-14 00:48:17 -0400

    Update node.h

--- node.h
+++ node.h
@@ -1,4 +1,6 @@
-
+/**john's node.h
+ * version x.x1
+ */
 
 #ifndef NODE_H
 #define NODE_H
@@ -12,15 +14,23 @@
 using namespace std;
 
 class node {
+    // Some form of addressing
     node * id
+    // List of links connected to the node
     vector<link *> links;
     
 public:
+    // Constructor
     node();
+    // Gets address of the node
     node * get_id();
+    // Add link to the node
     void add_link(link * new_link);
+    // Gets list of links connected to the node
     vector<link *> get_links();
-    link * get_link(node * destination);
+    // Gets the link connected to the given node
+    link * get_link(node * next_node);
+    // Sends given packet through the given link
     void send_pkt(packet * out_packet, link * out_link);
 };
 
