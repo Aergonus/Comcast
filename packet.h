c0ea7c863036e5d897866d8cc0adb5f88e8b2d40
Spellchaser <KangqiaoLei@gmail.com>
2016-04-14 20:40:00 -0400

    Derived DataPacket

--- packet.h
+++ packet.h
@@ -18,32 +18,43 @@ class packet{
     private:
         node *src;//sender of the packet(same as the sender of the flow)
         node *dst;//dstination of the packet (same as the dstination of the flow)
-        flow *pFlow;//parent flow 
 		packet_type type; // ENUM
 		int size; //size of the packet. Unit: bits
-        int seqNum; //The sequence number of the packet.
-		int ackNum; //The acknowledgement number of the packet. 
-		int killswitch;
+		int ks;
     public:
         //constructors
-        packet(node *src, node *dst, flow *f, packet_type type, int size, int seqNum, int ackNum, int ks)...
-			:src(src), dst(dst), pFlow(f), type(type), size(size), seqNum(seqNum), ackNum(ackNum), killswitch(killswitch);
+        packet(node *src, node *dst, packet_type type, int killswitch)...
+			:src(src), dst(dst), type(type), size(size), ks(killswitch);
 		
         //FUNCTIONS
         //returns the src node pointer
         node* getSrc(){return src;};
         //returns the dstination node pointer
         node* getDst(){return dst;};
-		flow* getFlow(){return pFlow;};
         //returns the size of the packet in bits
         int getSize(){return size;};
-        //returns the sequence number of the packet
+		//returns the ks number of the packet
+		int getKillSwitch(){return ks;};
+		//decrement Kill Switch
+		int perishSong(){return --ks;};
+};
+
+class data_pak : public packet{
+	private:
+		int seqNum; //The sequence number of the packet.
+		int ackNum; //The acknowledgement number of the packet. 
+		flow *pFlow;//parent flow 
+		
+	public: 
+		data_pak(node *src, node *dst, packet_type type, int killswitch, ...
+			int size, int seqNum, int ackNum, flow *f) : packet(src, dst, type, killswitch), pFlow(f), seqNum(seqNum), ackNum(ackNum);
+		
+		flow* getFlow(){return pFlow;};
+		//returns the sequence number of the packet
         int getSeqNum(){return seqNum;};
         //returns the ack number of the packet
         int getAckNum(){return ackNum;};
-		//returns the ks number of the packet
-		int getKillSwitch(){return killswitch;};
-		//decrement Kill Switch
-		int perishSong(){return --killswitch;};
-};
+}
+
+//TODO: Routing Packet
 #endif
