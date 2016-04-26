b84387058060c4e39bc730d86f7e3867d41ee179
HanEui <euiseonghan@gmail.com>
2016-04-26 19:00:19 -0400

    add router with Bellman-Ford Routing

--- router.cpp
+++ router.cpp
@@ -1,44 +1,64 @@
-/**
- * ECE408 
- * host.cpp
- * Purpose: 
- * 
- * @author Eric Nguyen, Kangqiao Lei
- * @version 0.2.0 04/19/16
- */
- 
-#include <iostream>
-#include <algorithm>
 #include "router.h"
-//#include "link.h"
-#include "flow.h"// Included because we call one of flow's functions
+#include "event.h"
+#include "event_manager.h"
 
-// Initialize and link the host to the network as an endpoint
-void router::addLink(Link* l){
-	// Hosts should only have one connection
-	assert(links.empty());
-	links.push_back(l);
+router::router(std::string id): Node(id){}
+
+void router::receive_pak(Packet *p){
+  if (p->type == CTRL){ //if the received packet is control type
+    receiveControl(p);
+  }
+  else{
+    get_route(p.getDst()) -> receive_pak(p); //pushes the packet to the link
+  }
+} 
+
+link* router::get_route(std::string host_id){
+  return next_hop_[host_id];
 }
 
-void router::addFlow(flow* f){
-	flows.push_back(f);
+//separate vectors for router and host?
+void router::update_table(std::string router_id){
+  routing_table[router_id] = dynamic_cast<router>(neighbors[router_id]).routing_vector();
+  updateCost();
 }
 
-// Upon receive_packet event, process and send to associated flow
-int router::receive_packet(packet* p){
-	// Add check if packet is routing, drop
-    std::vector<flow *>::iterator itr = std::find(flows.begin(), flows.end(), p->getFlow());
-    assert(!itr == flows.end());
-	(*itr)->receive_pak(p);
-	return 0;
+//Implementation of Bellman-Ford
+void router::update_cost(){ // updates cost vector every time step
+  for(auto &neighbor : links){  
+    //update the cost of each link associated with different neighbor
+    //neighbor.first = neighbor id
+    //neighbor.second = link pointer 
+    costVec[neighbor.first] = neighbor.second -> link_cost(); //Define Link Cost= sum (occupied buffer/rate) TODO: implement double link_cost() function in link.cpp
+  }
+  for(auto r : routing_table){ // r.first = router id, r.second = map<host id, cost> 
+    for(auto &c : r.second){ //c.first = host id, c.second = cost to the host
+      double temp = c.second + costVec[r.first];            //if updated cost is less then the original cost, copy.
+        if (temp < routing_table[Node::id_][c.first]){
+          routing_table[Node::id_][c.first] = temp;
+          next_hop[c.first] = r.first; //set next hop to be the link with the minimum total cost
+        }
+    }
+  }
 }
 
-Link* router::getLink(){
-  return l;
+//every 'x' time step, broadcast my routing table
+void router::send_control() {   
+  for(auto &neighbor : neighbors){
+    packet p* = new packet(this, neighbor.second, CTRL, 0); //TODO: killswitch default 0 okay?
+    neighbor.second -> receive_pak(p);
+  }
 }
 
-node* router::getConnectedNode(Link *connection){
-	return connection->getOtherNode(&this);
+//whenever the router receives ctrl packet, update the routing table
+void router::receive_control(packet* p){
+  update_table(p->getSrc().id());
 }
 
-// Debug print host name and address
+//returns the map<host id, cost from "me" to the host>
+std::map<std::string, double> router::routing_vector() {
+  return routing_table[node::id]; 
+}
+
+
+//initiate with greedy algorithm
--- router.h
+++ router.h
@@ -1,42 +1,43 @@
 /**
  * ECE408 
- * host.h
- * Purpose: Hosts represent access points.
- * They send packets from flows through Links to another Host.
+ * router.h
+ * Purpose: 
  * 
- * @author  Eui Han
+ * @author Eui Han, Arvind Nagalingam
  * @version 0.2.0 04/19/16
  */
-
 #ifndef ROUTER_H
 #define ROUTER_H
 
-#include <vector>
-#include <string>
-#include "node.h"
-
-class Link;
-class flow;
+class net;
 class packet;
+class link;
+#include "node.h"
+#include <map>
 
-class router : public node {
-	 private: 
-		std::vector<flow *> flows; 
-                Link* l;
-	 public: 
-		// The constructor creates the host, initially unconnected and without flows.
-		router(std::string name): node(name){};
-		
-		// Add a flow for the Host
-		int addFlow(flow *f);
-		Link* getLink();
-		// Sends a packet into the network via the host's link
-		// Called by the Flow and the TLA
-		//virtual bool send_Pak(packet);
-		
-		// Receives incoming packet
-		virtual int receive_packet(packet *p);
-                Link* getLink();
-                node* getConnectedNode(Link*);
+class router: public node{
+  public:
+    router(std::string id);
+    void receive_pak(packet *p);
+    link* get_route(std::string); // looks up the routing table and returns the link
+    void update_table(std::string); // updates the routing table every x time step
+    void update_cost();
+    void send_control() ;
+    void receive_control(packet* p);
+  private:
+    //each row represents each router's dist_ + cost_ vector
+    std::map<std::string, std::map<std::string, double> > routing_table;
+
+    //map <neighbor id, link cost>
+    std::map<std::string, double> costVec;
+    
+    //map <hst id, neighbor id>
+    std::map<std::string, std::string> next_hop;
+
+    //map <node id, pointer to Link>
+    std::map<std::string, link*> links;
+
+    //map <router id, pointer to the router>
+    std::map<std::string, router*> neighbors;
 };
 #endif
