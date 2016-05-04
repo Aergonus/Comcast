db32d3223ca4cda335211a935e31a221594c9e7b
Spellchaser <KangqiaoLei@gmail.com>
2016-05-04 07:57:03 -0400

    TestCase 0...0...0...
    I know how to count

--- Comcast.sh
+++ Comcast.sh
@@ -12,10 +12,10 @@ echo "Running Test Case 0"
 ./Comcast.exe -i ./input/test_case_0.json -o TC0_Output.csv -d TC0_debug.csv
 
 echo "Running Test Case 1"
-./Comcast.exe -i ./input/test_case_0.json -o TC1_Output.csv -d TC1_debug.csv
+./Comcast.exe -i ./input/test_case_1.json -o TC1_Output.csv -d TC1_debug.csv
 
 echo "Running Test Case 2"
-./Comcast.exe -i ./input/test_case_0.json -o TC2_Output.csv -d TC2_debug.csv
+./Comcast.exe -i ./input/test_case_2.json -o TC2_Output.csv -d TC2_debug.csv
 
 echo "Comcast has successfully extorted you for your $ $ $"
 echo "If you would like to donate more to Comcast please execute"
